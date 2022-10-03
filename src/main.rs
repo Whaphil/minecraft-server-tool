749844c6f1f7e871c3c5f5fc2939a1ebd1b33724
Philipp Sommer <philsommer2002@gmail.com>
2022-10-03 21:15:01 +0200

    added serde json

--- src/main.rs
+++ src/main.rs
@@ -5,12 +5,12 @@ mod server_starter;
 #[derive(Parser, Debug)]
 struct Cli {
     #[arg(index = 1)]
-    version: String,
+    path_to_jar: String,
     #[arg(long, short)]
     world_name: Option<String>,
 }
 
 fn main() {
     let args = Cli::parse();
-    server_starter::start_server("1.19.1", "1");
+    server_starter::start_server(&args.path_to_jar[..], "1");
 }
--- src/server_starter.rs
+++ src/server_starter.rs
@@ -1,6 +1,39 @@
-use std::process::{Command};
+use std::process::{Command, Output};
 
 use std::env;
+
+fn parse_output(output: Output) -> String {
+    match output.status.code() {
+        None => String::from("No exit code"),
+        Some(code) => match code {
+            0 => String::from_utf8(output.stdout).unwrap_or(String::from(
+                "Someting went wrong trying to parse the output",
+            )),
+            _ => String::from_utf8(output.stderr).unwrap_or(String::from(
+                "Somthing went wrong trying to parse the error message",
+            )),
+        },
+    }
+}
+
+fn run_jar_command(version_name: &str, world_name: &str) -> String {
+    let output = Command::new("java")
+        .args([
+            "-jar",
+            &format!("{}.jar", version_name)[..],
+            "--world",
+            &format!("{}", world_name)[..],
+        ])
+        .output();
+    match output {
+        Ok(output) => parse_output(output),
+        Err(_e) => String::from("Someting went wrong trying to run the program"),
+    }
+}
+
+fn get_debug_message(version_name: &str, world_name: &str) -> String {
+    format!("DEBUG mode is turned on, server will not be started.\n Values were: Version name: {} World name: {}", version_name,world_name)
+}
 
 pub fn start_server(version_name: &str, world_name: &str) {
     let debug = match env::var("DEBUG") {
@@ -9,16 +42,9 @@ pub fn start_server(version_name: &str, world_name: &str) {
     };
 
     let command_output: String = match !debug {
-        true => match 
-           Command::new("java")
-                .args(["-jar", &format!("{}.jar",version_name)[..], "--world", &format!("{}",world_name)[..]])
-                .output() {
-                    Ok(val) => val.status.to_string(),
-                    Err(error) => error.to_string()
-                }
-        
-        false => String::from(format!("DEBUG mode is turned on, server will not be started.\n Values were: Version name: {} World name: {}", version_name,world_name)),
+        true => run_jar_command(version_name, world_name),
+        false => get_debug_message(version_name, world_name),
     };
 
-    println!("{}",command_output);
+    println!("{}", command_output);
 }
